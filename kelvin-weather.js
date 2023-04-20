d9f14e9b4ea12eb4ebc6107f98e5b3fca1b66312
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-04-20 10:33:44 +0100

    Add files via upload

---
+++ kelvin-weather.js
@@ -0,0 +1,15 @@
+const kelvin = 0;
+console.log(kelvin);
+// temperature in kelvin
+let celsius;
+celsius = kelvin - 273;
+console.log(celsius);
+// temperature in celsius
+let fahrenheit;
+fahrenheit = (celsius*(9/5)+32); 
+console.log(fahrenheit);
+// temperature in fahrenheit
+Math.floor(fahrenheit);
+console.log(Math.floor(fahrenheit));
+// fahrenheit temperature rounded
+console.log(`The temperature is ${Math.floor(fahrenheit)} degrees Fahrenheit.`);
