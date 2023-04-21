59691b9c169416f76294e975418860eaad6675e8
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-04-21 13:39:05 +0100

    Add files via upload

---
+++ dog-years.js
@@ -0,0 +1,22 @@
+let myAge = 41;
+console.log(myAge);
+//my age
+let earlyYears = 2;
+// first 2 years of a dog's life
+earlyYears = earlyYears *10.5;
+console.log(earlyYears);
+let laterYears;
+laterYears = myAge -2;
+// myAge - earlyYears
+laterYears*=4;
+console.log(laterYears);
+//laterYears multiplied by 4
+let myAgeInDogYears;
+myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+// my age in dog years 
+let myName = "Letizia";
+console.log(myName.toLowerCase());
+// my name in lower case
+console.log(`My name is ${myName.toLowerCase()}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+//my age in dog years ;-)
